9f38a826fb043a40687fa69fbf58c21a26547da2
Your Name <you@example.com>
2021-11-21 18:25:45 +0600

    file-edit

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const port =process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
+// server
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.amixw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
